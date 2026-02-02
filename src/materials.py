a9a86cab384096a06bd32149e2cecd7305ec3fa3
Lorraine Delaroque <105422516+ldelaroque@users.noreply.github.com>
2026-02-02 17:24:37 +0100

    Refactor grain boundary and PR mechanism parameters

--- src/materials.py
+++ src/materials.py
@@ -18,16 +18,16 @@ def saline_ice():
         D_u_d = 1.2e-10,
         b     = 4.52e-10,             # in m, Burger's vector. NB. depends on the crystalline structure
         # -- Grain boundary parameters --
-        tau_gb_0 = 8e-28,            # in s, relaxation time
-        Q_gb     = 1.32*conv,        # in eV, activation energy for grain-boundary. Provient orginellement de l'article de Tatibouret (1987) 
-                                     # sur de la glace pure, car pas de valeur fiable pour la glace saline selon Cole (1995)
-        dD_gb    = 3e-11,            # in Pa⁻¹, max amplil
-        a_gb     = 0.6,              # Debye-type peak width at HF
+        tau_gb_0 = 8e-28,             # in s, relaxation time
+        Q_gb     = 1.32*conv,         # in eV, activation energy for grain-boundary. Provient orginellement de l'article de Tatibouret (1987) 
+                                      # sur de la glace pure, car pas de valeur fiable pour la glace saline selon Cole (1995)
+        dD_gb    = 3e-11,             # in Pa⁻¹, max amplil
+        a_gb     = 0.6,               # Debye-type peak width at HF
         D_u_gb   = 1.5e-10,
         # -- PR mechanism parameters --
-        Em   = 35e3,                 # J/mol, Bjerrum detects'energies of migration
-        Ef   = 25e3,                 # J/mol, Bjerrum detects'energies of formation
-        Ep   = 25e3,                 # in J/mol (for tanδ_max empirical)
-        tau0 = 6e-16,                # in s [Tatibouet et al. 1981]
-        c0   = 1e-7                  # reference concentration of extrinsic defects
+        Em       = 35e3,              # J/mol, Bjerrum detects'energies of migration
+        Ef       = 25e3,              # J/mol, Bjerrum detects'energies of formation
+        Ep       = 25e3,              # in J/mol (for tanδ_max empirical)
+        tau_pr_0 = 6e-16,             # in s [Tatibouet et al. 1981]
+        c0       = 1e-7               # reference concentration of extrinsic defects
     )
